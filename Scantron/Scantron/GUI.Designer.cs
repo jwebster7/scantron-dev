cf1bfb286195f4d585a0758ea0409bcd12a63dc4
Joseph Webster <jwebster7@ksu.edu>
2017-12-20 15:13:49 -0600

    Base template for the GUI, buttons and properties set.
    GUI incomplete

--- Scantron/Scantron/GUI.Designer.cs
+++ Scantron/Scantron/GUI.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.uxReadFiles = new System.Windows.Forms.Button();
+            this.uxCreateFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uxReadFiles
+            // 
+            this.uxReadFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.uxReadFiles.Location = new System.Drawing.Point(12, 12);
+            this.uxReadFiles.Name = "uxReadFiles";
+            this.uxReadFiles.Size = new System.Drawing.Size(200, 45);
+            this.uxReadFiles.TabIndex = 0;
+            this.uxReadFiles.Text = "Read Scantron Sheets";
+            this.uxReadFiles.UseVisualStyleBackColor = true;
+            // 
+            // uxCreateFile
+            // 
+            this.uxCreateFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.uxCreateFile.Location = new System.Drawing.Point(12, 63);
+            this.uxCreateFile.Name = "uxCreateFile";
+            this.uxCreateFile.Size = new System.Drawing.Size(200, 45);
+            this.uxCreateFile.TabIndex = 1;
+            this.uxCreateFile.Text = "Generate Answer File";
+            this.uxCreateFile.UseVisualStyleBackColor = true;
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(226, 122);
+            this.Controls.Add(this.uxCreateFile);
+            this.Controls.Add(this.uxReadFiles);
+            this.Name = "GUI";
+            this.Text = "File Generator";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button uxReadFiles;
+        private System.Windows.Forms.Button uxCreateFile;
     }
 }
 
