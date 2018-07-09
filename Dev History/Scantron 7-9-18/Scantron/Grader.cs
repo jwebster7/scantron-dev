238d760dc2ed8bec313d2ff63e7b8e67b843b040
schweer <schweer@ksu.edu>
2018-07-09 14:27:25 -0500

    Saved 7-9-18 version because we are redoing the program.

---
+++ Dev History/Scantron 7-9-18/Scantron/Grader.cs
@@ -0,0 +1,120 @@
+﻿// Grader.cs
+//
+// Property of the Kansas State University IT Help Desk
+// Written by: William McCreight, Caleb Schweer, and Joseph Webster
+// 
+// An extensive explanation of the reasoning behind the architecture of this program can be found on the github 
+// repository: https://github.com/prometheus1994/scantron-dev/wiki
+//
+// This class handles button click events on the GUI.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scantron
+{
+    class Grader
+    {
+        // Holds the students to be graded.
+        private List<Student> students = new List<Student>();
+        // Holds the answer key to compare to student responses.
+        private string[] answer_key = new string[5];
+        // Hold a list of which questions are given partial credit.
+        private bool[] partial_credit;
+
+        // Default constructor for the grader.
+        public Grader()
+        {
+
+        }
+        
+        // Real constructor for the grader.
+        public Grader(List<Student> students, string[] answer_key, bool[] partial_credit)
+        {
+            this.students = students;
+            this.answer_key = answer_key;
+            this.partial_credit = partial_credit;
+        }
+
+        // Method for grading the student responses.
+        public void Grade()
+        {
+            foreach (Student student in students)
+            {
+                student.Grade = CheckAnswers(student.Answers, answer_key);
+            }
+        }
+
+        // Check student answers against the answer key. Canvas grading
+        private float[] CheckAnswers(string[] answers, string[] answer_key)
+        {
+            int number_of_questions = answer_key[0].Length;
+            float[] score = new float[number_of_questions];
+
+            for (int i = 0; i < number_of_questions; i++)
+            {
+                if (partial_credit[i])
+                {
+                    float total_answers = 0;
+                    float correct_answers = 0;
+                    float incorrect_answers = 0;
+
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (answer_key[j][i] != ' ')
+                        {
+                            total_answers++;
+
+                            if (answers[j][i] == answer_key[j][i])
+                            {
+                                correct_answers++;
+                            }
+                        }
+                        else
+                        {
+                            if (answers[j][i] != answer_key[j][i])
+                            {
+                                incorrect_answers++;
+                            }
+                        }
+                    }
+
+                    score[i] = correct_answers / total_answers - incorrect_answers / total_answers;
+                }
+                else
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (answers[j][i] != answer_key[j][i])
+                        {
+                            score[i] = 0;
+                            break;
+                        }
+                        score[i] = 1;
+                    }
+                }
+            }
+
+            return score;
+        }
+
+        // Convert the students' grades into a CSV file to be uploaded to the Canvas gradebook.
+        public override string ToString()
+        {
+            string info = "Student,ID,SIS User ID,SIS Login ID,Section," + answer_key[0].Length + Environment.NewLine +
+                            "Points Possible,,,,," + answer_key[0].Length + Environment.NewLine;
+            int count = 1;
+
+            foreach (Student student in students)
+            {
+                count++;
+                info += "Scantron Card(s): " + count + ",," + student.WID + ",,," + student.Grade.Sum();
+            }
+
+            return info;
+        }
+    }
+}
---
+++ Dev History/Scantron 7-9-18/Scantron/Scantron.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Scantron
+{
+    partial class Scantron
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxStart = new System.Windows.Forms.Button();
+            this.uxInstructionBox = new System.Windows.Forms.TextBox();
+            this.uxStop = new System.Windows.Forms.Button();
+            this.uxCreateFile = new System.Windows.Forms.Button();
+            this.uxAdmin = new System.Windows.Forms.Button();
+            this.uxCanConvert = new System.Windows.Forms.Button();
+            this.uxRestart = new System.Windows.Forms.Button();
+            this.uxMainPanel = new System.Windows.Forms.Panel();
+            this.uxAnswerKeyPanel = new System.Windows.Forms.Panel();
+            this.uxNumberOfQuestions = new System.Windows.Forms.TextBox();
+            this.uxEnter = new System.Windows.Forms.Button();
+            this.uxCreateAnswerKey = new System.Windows.Forms.Button();
+            this.Grade = new System.Windows.Forms.Button();
+            this.uxStudentAnswerPanel = new System.Windows.Forms.Panel();
+            this.uxStudentSelector = new System.Windows.Forms.ComboBox();
+            this.uxMainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // uxStart
+            // 
+            this.uxStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.5F);
+            this.uxStart.Location = new System.Drawing.Point(3, 279);
+            this.uxStart.Name = "uxStart";
+            this.uxStart.Size = new System.Drawing.Size(85, 65);
+            this.uxStart.TabIndex = 0;
+            this.uxStart.Tag = "";
+            this.uxStart.Text = "Start";
+            this.uxStart.UseVisualStyleBackColor = true;
+            this.uxStart.Click += new System.EventHandler(this.uxStart_Click);
+            // 
+            // uxInstructionBox
+            // 
+            this.uxInstructionBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.uxInstructionBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.5F);
+            this.uxInstructionBox.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.uxInstructionBox.Location = new System.Drawing.Point(3, 28);
+            this.uxInstructionBox.MaximumSize = new System.Drawing.Size(580, 245);
+            this.uxInstructionBox.Multiline = true;
+            this.uxInstructionBox.Name = "uxInstructionBox";
+            this.uxInstructionBox.ReadOnly = true;
+            this.uxInstructionBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.uxInstructionBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.uxInstructionBox.Size = new System.Drawing.Size(351, 245);
+            this.uxInstructionBox.TabIndex = 3;
+            this.uxInstructionBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // uxStop
+            // 
+            this.uxStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.5F);
+            this.uxStop.Location = new System.Drawing.Point(3, 350);
+            this.uxStop.Name = "uxStop";
+            this.uxStop.Size = new System.Drawing.Size(85, 65);
+            this.uxStop.TabIndex = 4;
+            this.uxStop.Text = "Stop";
+            this.uxStop.UseVisualStyleBackColor = true;
+            this.uxStop.Click += new System.EventHandler(this.uxStop_Click);
+            // 
+            // uxCreateFile
+            // 
+            this.uxCreateFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.uxCreateFile.Location = new System.Drawing.Point(94, 279);
+            this.uxCreateFile.Name = "uxCreateFile";
+            this.uxCreateFile.Size = new System.Drawing.Size(130, 65);
+            this.uxCreateFile.TabIndex = 5;
+            this.uxCreateFile.Text = "Create File (Canvas)";
+            this.uxCreateFile.UseVisualStyleBackColor = true;
+            this.uxCreateFile.Click += new System.EventHandler(this.uxCreateFile_Click);
+            // 
+            // uxAdmin
+            // 
+            this.uxAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.uxAdmin.Location = new System.Drawing.Point(3, 3);
+            this.uxAdmin.Name = "uxAdmin";
+            this.uxAdmin.Size = new System.Drawing.Size(53, 22);
+            this.uxAdmin.TabIndex = 6;
+            this.uxAdmin.Text = "Admin";
+            this.uxAdmin.UseVisualStyleBackColor = true;
+            this.uxAdmin.Click += new System.EventHandler(this.uxAdmin_Click);
+            // 
+            // uxCanConvert
+            // 
+            this.uxCanConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.uxCanConvert.Location = new System.Drawing.Point(94, 350);
+            this.uxCanConvert.Name = "uxCanConvert";
+            this.uxCanConvert.Size = new System.Drawing.Size(130, 65);
+            this.uxCanConvert.TabIndex = 7;
+            this.uxCanConvert.Text = "Create File (CanConvert)";
+            this.uxCanConvert.UseVisualStyleBackColor = true;
+            this.uxCanConvert.Click += new System.EventHandler(this.uxCanConvert_Click);
+            // 
+            // uxRestart
+            // 
+            this.uxRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.5F);
+            this.uxRestart.Location = new System.Drawing.Point(230, 311);
+            this.uxRestart.Name = "uxRestart";
+            this.uxRestart.Size = new System.Drawing.Size(124, 65);
+            this.uxRestart.TabIndex = 8;
+            this.uxRestart.Text = "Restart";
+            this.uxRestart.UseVisualStyleBackColor = true;
+            this.uxRestart.Click += new System.EventHandler(this.uxRestart_Click);
+            // 
+            // uxMainPanel
+            // 
+            this.uxMainPanel.Controls.Add(this.uxStart);
+            this.uxMainPanel.Controls.Add(this.uxRestart);
+            this.uxMainPanel.Controls.Add(this.uxInstructionBox);
+            this.uxMainPanel.Controls.Add(this.uxCanConvert);
+            this.uxMainPanel.Controls.Add(this.uxStop);
+            this.uxMainPanel.Controls.Add(this.uxAdmin);
+            this.uxMainPanel.Controls.Add(this.uxCreateFile);
+            this.uxMainPanel.Location = new System.Drawing.Point(-1, 0);
+            this.uxMainPanel.Name = "uxMainPanel";
+            this.uxMainPanel.Size = new System.Drawing.Size(363, 422);
+            this.uxMainPanel.TabIndex = 9;
+            // 
+            // uxAnswerKeyPanel
+            // 
+            this.uxAnswerKeyPanel.AutoScroll = true;
+            this.uxAnswerKeyPanel.Location = new System.Drawing.Point(368, 0);
+            this.uxAnswerKeyPanel.Name = "uxAnswerKeyPanel";
+            this.uxAnswerKeyPanel.Size = new System.Drawing.Size(442, 349);
+            this.uxAnswerKeyPanel.TabIndex = 10;
+            // 
+            // uxNumberOfQuestions
+            // 
+            this.uxNumberOfQuestions.Location = new System.Drawing.Point(368, 355);
+            this.uxNumberOfQuestions.Name = "uxNumberOfQuestions";
+            this.uxNumberOfQuestions.Size = new System.Drawing.Size(100, 20);
+            this.uxNumberOfQuestions.TabIndex = 11;
+            // 
+            // uxEnter
+            // 
+            this.uxEnter.Location = new System.Drawing.Point(368, 381);
+            this.uxEnter.Name = "uxEnter";
+            this.uxEnter.Size = new System.Drawing.Size(75, 23);
+            this.uxEnter.TabIndex = 12;
+            this.uxEnter.Text = "Enter";
+            this.uxEnter.UseVisualStyleBackColor = true;
+            this.uxEnter.Click += new System.EventHandler(this.uxEnter_Click);
+            // 
+            // uxCreateAnswerKey
+            // 
+            this.uxCreateAnswerKey.Location = new System.Drawing.Point(368, 410);
+            this.uxCreateAnswerKey.Name = "uxCreateAnswerKey";
+            this.uxCreateAnswerKey.Size = new System.Drawing.Size(75, 39);
+            this.uxCreateAnswerKey.TabIndex = 13;
+            this.uxCreateAnswerKey.Text = "Create Answer Key";
+            this.uxCreateAnswerKey.UseVisualStyleBackColor = true;
+            this.uxCreateAnswerKey.Click += new System.EventHandler(this.uxCreateAnswerKey_Click);
+            // 
+            // Grade
+            // 
+            this.Grade.Location = new System.Drawing.Point(368, 455);
+            this.Grade.Name = "Grade";
+            this.Grade.Size = new System.Drawing.Size(75, 23);
+            this.Grade.TabIndex = 14;
+            this.Grade.Text = "Grade";
+            this.Grade.UseVisualStyleBackColor = true;
+            this.Grade.Click += new System.EventHandler(this.Grade_Click);
+            // 
+            // uxStudentAnswerPanel
+            // 
+            this.uxStudentAnswerPanel.AutoScroll = true;
+            this.uxStudentAnswerPanel.Location = new System.Drawing.Point(816, 0);
+            this.uxStudentAnswerPanel.Name = "uxStudentAnswerPanel";
+            this.uxStudentAnswerPanel.Size = new System.Drawing.Size(440, 349);
+            this.uxStudentAnswerPanel.TabIndex = 15;
+            // 
+            // uxStudentSelector
+            // 
+            this.uxStudentSelector.FormattingEnabled = true;
+            this.uxStudentSelector.Location = new System.Drawing.Point(904, 355);
+            this.uxStudentSelector.Name = "uxStudentSelector";
+            this.uxStudentSelector.Size = new System.Drawing.Size(153, 21);
+            this.uxStudentSelector.TabIndex = 16;
+            this.uxStudentSelector.Text = "Choose Student...";
+            this.uxStudentSelector.SelectedIndexChanged += new System.EventHandler(this.uxStudentSelector_SelectedIndexChanged);
+            // 
+            // Scantron
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1256, 668);
+            this.Controls.Add(this.uxStudentSelector);
+            this.Controls.Add(this.uxStudentAnswerPanel);
+            this.Controls.Add(this.Grade);
+            this.Controls.Add(this.uxCreateAnswerKey);
+            this.Controls.Add(this.uxEnter);
+            this.Controls.Add(this.uxNumberOfQuestions);
+            this.Controls.Add(this.uxAnswerKeyPanel);
+            this.Controls.Add(this.uxMainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(600, 375);
+            this.Name = "Scantron";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "File Generator";
+            this.uxMainPanel.ResumeLayout(false);
+            this.uxMainPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uxStart;
+        private System.Windows.Forms.TextBox uxInstructionBox;
+        private System.Windows.Forms.Button uxStop;
+        private System.Windows.Forms.Button uxCreateFile;
+        private System.Windows.Forms.Button uxAdmin;
+        private System.Windows.Forms.Button uxCanConvert;
+        private System.Windows.Forms.Button uxRestart;
+        private System.Windows.Forms.Panel uxMainPanel;
+        private System.Windows.Forms.Panel uxAnswerKeyPanel;
+        private System.Windows.Forms.TextBox uxNumberOfQuestions;
+        private System.Windows.Forms.Button uxEnter;
+        private System.Windows.Forms.Button uxCreateAnswerKey;
+        private System.Windows.Forms.Button Grade;
+        private System.Windows.Forms.Panel uxStudentAnswerPanel;
+        private System.Windows.Forms.ComboBox uxStudentSelector;
+    }
+}
+
