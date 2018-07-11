6c863b57dddb7589a3e2a90003dcac4ce32e78e9
Joseph Webster <jwebster7@ksu.edu>
2018-07-11 16:30:06 -0500

    Student.cs added. Card.cs~HEAD removed. Hopefully doesn't break our repo...

--- scantron/scantron/student.cs
+++ scantron/scantron/student.cs
@@ -7,26 +7,5 @@ namespace Scantron
 {
     class Student
     {
-        private string wid;
-
-        Dictionary<int, Card> response = new Dictionary<int, Card>();
-
-        public string WID
-        {
-            get
-            {
-                return wid;
-            }
-        }
-
-        public Dictionary<int, Card> Response
-        {
-            get
-            {
-                return response;
-            }
-        }
-
-
     }
 }
