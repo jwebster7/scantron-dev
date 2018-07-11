e85622dfc3031dfb83eb8a1bc28b903fbbb8db35
Joseph Webster <jwebster7@ksu.edu>
2018-07-11 16:45:13 -0500

    Student.cs once again

--- scantron/scantron/student.cs
+++ scantron/scantron/student.cs
@@ -7,6 +7,36 @@ namespace Scantron
 {
     class Student
     {
+        // the students wid
+        private string wid;
 
+        // the students Scantron card(s); Holds 1 KVP if only question count is <= 50
+        private Dictionary<int, Card> cards = new Dictionary<int, Card>();
+
+        // getter/setter for wid
+        public string WID
+        {
+            get
+            {
+                return wid;
+            }
+            set
+            {
+                this.wid = value;
+            }
+        }
+
+        // getter/setter for cards
+        public Dictionary<int, Card> Cards
+        {
+            get
+            {
+                return cards;
+            }
+            set
+            {
+                this.cards = value;
+            }
+        }
     }
 }
