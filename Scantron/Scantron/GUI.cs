0ec0317ec373f4a542aaea9ba0541e1212e2b433
Joseph Webster <jwebster7@ksu.edu>
2018-07-30 14:36:54 -0500

    more white space changes

--- Scantron/Scantron/GUI.cs
+++ Scantron/Scantron/GUI.cs
@@ -362,7 +362,6 @@ namespace Scantron
                 FilterIndex = 1
             };
 
-
             if (uxSaveFileDialog.ShowDialog() == DialogResult.OK)
             {
                 string path = uxSaveFileDialog.FileName;
